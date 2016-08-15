d299a93650c1a72170a97f80aa023f4eec8e30f1
manni09 <manuelkenna@gmail.com>
2016-08-15 17:39:54 -0500

    intial

---
+++ src/main/java/com/example/ExtraCreditWeekOneApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtraCreditWeekOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtraCreditWeekOneApplication.class, args);
+	}
+}
